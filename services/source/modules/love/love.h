f5b27a966859b207eb1f26b11815659452703f34
simonb <>
2002-07-14 10:45:12 +0000

    Some cleanups.. (calm before the storm?)

--- services/source/modules/love/love.h
+++ services/source/modules/love/love.h
@@ -6,56 +6,44 @@
 */
 
 #ifndef __LOVE_H_
-#define __LOVE_H_
+# define __LOVE_H_ 1
 
-#include "kineircd/str.h"
-# define LOVE_FUNC(x)           x(Kine::String &origin, Kine::StringTokens &tokens)
+# include <kineircd/str.h>
 
-using Kine::String;
+# define LOVE_FUNC(x) \
+	void x(const Kine::String &origin, const Kine::StringTokens &tokens)
 
-#include "exordium/service.h"
-
-namespace Exordium {
+# include "exordium/service.h"
 
 
-class Love : public Service
-{
-private:
-  struct functionTableStruct
-  {
-    char const *command;
-    void LOVE_FUNC ((*function));
-  };
-  static struct functionTableStruct const functionTable[];
+class Love : public Exordium::Service {
+ private:
+   struct functionTableStruct {
+      const char *command;
+      const LOVE_FUNC((*function));
+   } static const functionTable[];
+   
+ public:
+   // Our constructor
+   Love(void)
+     {
+	std::cout << 
+	  "Why hello there, I'm a loving instance :) :)" << std::endl;
+     };
 
-public:
-  Love(void)
-   : Service()
-   {
-	std::cout << "Hi im an instance. :(" << std::endl;
-   };
-  ~Love(void) 
-   {
-	std::cout << "YOU BASTARD, YOU KILLED LOVE" << std::endl;
-   };   
-  void parseLine (String const &, String const &);
-  void parseLine (String const &, String const &, String const &);
+   // Our destructor
+   ~Love(void) 
+     { 
+	std::cout << "Share the love, don't trample on it :(" << std::endl; 
+     };
+   
+   // Parser for incoming stuff sent on a channel
+   void parseLine(const Kine::String &line, const Kine::String &origin,
+		  const Kine::String &channel)
+     { /* Nothing! Bwa ha ha ha hahahahaHEHhEHahehaheAEhaehHAEhaE!!! */ };
+   
+   // Parser for incoming stuff sent directly to us
+   void parseLine(const Kine::String &line, const Kine::String &origin);
 };
 
-
-
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif // __LOVE_H_
--- services/source/modules/love/module.cpp
+++ services/source/modules/love/module.cpp
@@ -5,48 +5,72 @@
  $Id$
 */
 
-#include "include/love.h"
-#include "exordium/service.h"
-#include "exordium/services.h"
-#include "kineircd/str.h"
-#include "exordium/module.h"
-#include <map>
+// Define if we are debugging
+#ifndef DEBUG
+# define DEBUG 1
+#endif
 
+#include "include/love.h"
+
+#include <exordium/service.h>
+#include <exordium/services.h>
+#include <exordium/module.h>
+
+#include <map>
+#include <sstream>
+
+// Use the stuff from the KineIRCd library using lovely short form
 using Kine::String;
 using Kine::StringTokens;
+
+/* We are in the exordium namespace, or supposed to be anyway.. I don't know,
+ * I'm too busy rebelling from the Wilkins Regime :(
+ */
 using namespace Exordium;
-struct Love::functionTableStruct const
-  Love::functionTable[] = {
-  {0}
-};
-void
-Love::parseLine (String const &line, String const &requestor, String const &ch)
-{
-return;
-}
-void 
-Love::parseLine (String const &line, String const &requestor)
-{   
-  StringTokens st (line);
-  String origin = requestor;
-  String command = st.nextToken ().toLower ();
-  for (int i = 0; functionTable[i].command != 0; i++)
-    {
-	Services::Debug(line);
-	Services::Debug(requestor);
-      // Does this match?   
-      if (command == functionTable[i].command)
-        {
-          // Run the command and leave
-          functionTable[i].function (origin, st);
-          return;
-        }
-    }
-  Services::serviceNotice ("Unrecognized Command", "Love", requestor);
-}
 
 
+/* service_init - Register ourselves to the wonderous enigma that is the core
+ * Original 13/07/2002 james
+ * Note: Using C-style symbols for dlsym()
+ */
 extern "C" Module *service_init(void) {
    return new Module("love", new Love());
 }
 
+
+// Our lovely list of commands in all their glorious ascii beauty
+const Love::functionTableStruct Love::functionTable[] = {
+     { 0 }
+};
+
+
+/* parseLine - Parse an incoming message (which was sent directly to us)
+ * Original 13/07/2002 james
+ */
+void Love::parseLine(const String &line, const String &origin)
+{   
+   // Start breaking up the line
+   StringTokens st(line);
+   String command = st.nextToken().toLower();
+  
+   // Run through the list of commands to find a match
+   for (int i = 0; functionTable[i].command != 0; i++) {
+#ifdef DEBUG
+      ostringstream debugLine;
+      debugLine << "Love::parseLine() -> '" << line << "' from " << origin;
+      Services::Debug(debugLine.str());
+#endif
+      
+      // Does this match?   
+      if (command == functionTable[i].command) {
+	 // Run the command and leave early
+	 functionTable[i].function(origin, st);
+	 return;
+      }
+   }
+   
+#ifdef DEBUG
+   // Bitch and moan.. bitch and moan..
+   Services::serviceNotice("Unrecognised Command", "Love", origin);
+#endif
+}
