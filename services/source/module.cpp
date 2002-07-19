b6ea8b83cd237e4774aa5cd3905d36931078469f
simonb <>
2002-07-19 19:58:14 +0000

    Added the start of the mod_exordium stuff.. woop woop

---
+++ services/source/module.cpp
@@ -0,0 +1,80 @@
+/*   
+ This file is a part of Exordium Network Services - (c) 2002 IRCDome Development Team                           $
+ $Author$
+ $Date$
+ $Id$
+*/
+
+#include <iostream>
+#include <exordium/services.h>
+#include <exordium/log.h>
+#include <exordium/sql.h>
+#include <kineircd/module-service.h>
+
+
+using namespace Exordium;
+
+// called just before the module is actually going to be used
+KINE_MODULE_START(moduleStart)
+{
+   cout << "moduleStart()" << endl;
+
+   // My, it looks an awful lot like main.cpp from here on... :) This is
+   // temporary, naturally.
+        Sql db;
+	Services me;
+	Log::init();
+	Log::logLine("Services started, beginning initalisation");
+	me.load_config();
+	db.init(me.mysqlHost,me.mysqlUser,me.mysqlPass,me.mysqlDb);
+	me.init();
+	me.run();
+	Log::logLine("Services terminated - Normal exit");
+   exit(0); // we are naughty using this here..
+}
+
+
+// called just before unloading the module
+KINE_MODULE_STOP(moduleStop)
+{
+   cout << "moduleStop()" << endl;
+}
+
+
+// information about ourselves
+static const Kine::Module::basicInfo_type moduleInfo = {
+   // List our version/copyright information (fill this in with defines James)
+   "exordium", // short name of the module, usually one word
+   "Exordium IRCDome Network Services", // long name of the module
+   "Copyright (c) 2002 IRCDome Development Team", // Copyright information
+   0, // Major version number
+   1, // Minor version number
+   2, // Patch-level (may be set to 0 if none)
+   ".beta", // extra information (may be set to null or 0 if none)
+
+   /* Note that I set up the version numbers above on purpose to illustrate
+    * how the version fields come together. The above comes out as follows:
+    *     exordium-0.1.2.beta
+    * If ".beta" was replaced with 0, it would look like this:
+    *     exordium-0.1.2
+    * If then you replaced the patch-level with 0, it would look like this:
+    *     exordium-0.1
+    * The other fields are always visible. The full name is used in other
+    * places.. same with the copyright.. probably on module loading or 
+    * something similar *shrugs*
+    */ 
+     
+   // Flags.. ignore this for now
+   Kine::Module::basicInfo_type::FLAG_NONE,
+     
+   // Our start and stop functions
+   &moduleStart,
+   &moduleStop
+};
+
+
+// called when the module is initially loaded
+KINE_MODULE_INIT
+{
+   return new Kine::ModuleService(moduleInfo);
+}
