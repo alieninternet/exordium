517ffa061756c0a21a9763c6a5c286eb0fe6fe1b
josullivan <>
2002-11-01 19:33:20 +0000

    This is a singleton factory for creating ChannelGame's.

---
+++ services/source/modules/mod.game/factory.h
@@ -0,0 +1,85 @@
+/* $Id: 
+ * 
+ * Exordium Network Services
+ * Copyright (C) 2002 IRCDome Development Team
+ * Copyright (c) 2002 Simon Butcher <pickle@alien.net.au>
+ *
+ * This file is a part of Exordium.
+ * 
+ * Exordium is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Exordium is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Exordium; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For contact details for the IRCDome Development Team please see the
+ * enclosed README file.
+ *
+ */
+
+#ifndef _SOURCE_MODULES_GAME_FACTORY_H_
+# define _SOURCE_MODULES_GAME_FACTORY_H_ 1
+
+# include <map>
+
+class ChannelGame;
+
+namespace Game {
+  typedef ChannelGame* (*GameCallback)();
+
+  class Factory {
+    private:
+      typedef std::map<const AISutil::String, GameCallback> CallbackMap;
+      CallbackMap callbacks;
+
+      Factory() { }
+      Factory(const Factory&) { };
+      Factory& operator = (const Factory&) { return *this; }
+
+    public:
+      static Factory& Instance()
+      {
+        static Factory factory;
+        return factory;
+      }
+      ~Factory() { }
+
+      bool registerType(const AISutil::String& name, GameCallback createFn)
+      {
+        return callbacks.insert(CallbackMap::value_type(name, createFn)).second;
+      };
+
+      bool unregisterType(const AISutil::String& name)
+      {
+        return callbacks.erase(name);
+      };
+
+      ChannelGame* createGame(const AISutil::String& name)
+      {
+        CallbackMap::const_iterator iter = callbacks.find(name);
+        if(iter == callbacks.end())
+        {
+#ifdef DEBUG
+          std::cerr << "Game " << name << "not found\n";
+#endif
+          return 0;
+        }
+
+        ChannelGame* game = iter->second();
+        return game;
+      };
+  };
+}; // Namespace Game
+
+// Complete the forwarded declaration
+# include "channelgame.h"
+   
+#endif // _SOURCE_MODULES_GAME_FACTORY_H_
