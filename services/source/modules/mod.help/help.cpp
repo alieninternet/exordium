57618df1994265cfd2bb02e367f4d415591640d8
ircdome <>
2003-03-19 10:50:55 +0000

    Added Praetorians help module, and enabled it to be built by default by configure

---
+++ services/source/modules/mod.help/help.cpp
@@ -0,0 +1,93 @@
+/* $Id$
+ * 
+ * Exordium Network Services
+ * Copyright (C) 2002 IRCDome Development Team
+ *
+ * This file is a part of Exordium.
+ * 
+ * Exordium is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Exordium is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Exordium; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For contact details for the IRCDome Development Team please see the
+ * enclosed README file.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "autoconf.h"
+#endif
+
+#include "help.h"
+#include <exordium/service.h>
+#include <exordium/services.h>
+#include <kineircd/str.h>
+
+
+using AISutil::String;
+using AISutil::StringTokens;
+using namespace Exordium::HelpModule;
+
+
+const Module::functionTableStruct Module::functionTable[] =
+{
+     { "help",		&Module::parseHELP },
+     { 0, 0 }
+};
+
+void Module::parseLine(StringTokens& line, User& origin, const bool safe)
+{
+   String command = line.nextToken ().toLower ();
+   for (int i = 0; functionTable[i].command != 0; i++) {
+      // Does this match?
+      if (command == functionTable[i].command) {
+         (this->*(functionTable[i].function))(origin, line);
+         return;
+      }
+   }
+   origin.sendMessage("Unrecognised Command", getName());
+}
+
+HELP_FUNC(Module::parseHELP)
+{
+   String word = tokens.nextToken();
+   String parm = tokens.nextToken();
+   services->doHelp(origin,getName(),word,parm);
+}
+
+EXORDIUM_SERVICE_INIT_FUNCTION
+{ return new Module(); }
+
+
+// Module information structure
+const Module::moduleInfo_type Module::moduleInfo = {
+   "Help Service",
+     0, 0,
+     Exordium::Service::moduleInfo_type::Events::NONE
+};
+
+
+// Start the service
+bool Module::start(Exordium::Services& s)
+{
+   // Set the services field appropriately
+   services = &s;
+
+   // Register ourself to the network
+   services->registerService(getName(), getName(),
+			     getConfigData().getHostname(),
+			     getConfigData().getDescription());
+   
+   // We started okay :)
+   return true;
+}
---
+++ services/source/modules/mod.help/help.h
@@ -0,0 +1,86 @@
+/* $Id$
+ * 
+ * Exordium Network Services
+ * Copyright (C) 2002 IRCDome Development Team
+ *
+ * This file is a part of Exordium.
+ * 
+ * Exordium is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Exordium is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Exordium; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For contact details for the IRCDome Development Team please see the
+ * enclosed README file.
+ *
+ */
+
+#ifndef _SOURCE_MODULES_HELP_HELP_H_
+# define _SOURCE_MODULES_HELP_HELP_H_ 1
+
+#include <exordium/service.h>
+#include <exordium/services.h>
+#include <kineircd/str.h>
+
+#define HELP_FUNC(x) \
+     void x(Exordium::User& origin, AISutil::StringTokens& tokens)
+
+
+namespace Exordium {
+   namespace HelpModule {
+      class Module : public Exordium::Service {
+       private:
+	 // Module information structure
+	 static const Exordium::Service::moduleInfo_type moduleInfo;
+	 
+	 // Configuration data class
+	 Exordium::Service::ConfigData configData;
+	 
+	 struct functionTableStruct {
+	    char const* const command;
+	    HELP_FUNC((Module::* const function));
+	 } static const functionTable[];
+
+       public:
+	 Module(void)
+	   : configData(moduleInfo.fullName, "somewhere.org", "Help")
+	   {};
+	 
+	 ~Module(void)
+	   {};
+	 
+	 // Start the module
+	 bool start(Exordium::Services& s);
+	 
+	 void parseLine(AISutil::StringTokens& line, Exordium::User& origin,
+			const bool safe);
+	 void parseLine(AISutil::StringTokens& line, Exordium::User& origin,
+			const AISutil::String& channel)
+	   {};
+	 
+	 // Grab the information structure of a module
+	 virtual const moduleInfo_type& getModuleInfo(void) const
+	   { return moduleInfo; };
+	 
+	 const Exordium::Service::ConfigData& getConfigData(void) const
+	   { return configData; };
+	 
+	 Exordium::Service::ConfigData& getConfigData(void)
+	   { return configData; };
+	 
+       private:
+	 HELP_FUNC(parseHELP);
+      }; // class Module
+   }; // namespace HelpModule
+}; // namespace Exordium
+
+#endif // _SOURCE_MODULES_HELP_HELP_H_
